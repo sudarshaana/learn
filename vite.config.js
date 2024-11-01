3308a98276173b2417765e23a2fc187579052021
sudarshaana <sudarshaana@gmail.com>
2024-11-01 16:59:59 +0600

    github page config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.csv']
+  assetsInclude: ['**/*.csv'],
+  base: '/learn/',
+
 })
