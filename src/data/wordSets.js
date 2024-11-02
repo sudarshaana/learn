1b3a795df413a975a3a356df4353b799828ea36f
sudarshaana <sudarshaana@gmail.com>
2024-11-02 12:28:54 +0600

    added new word list to the item

--- src/data/wordSets.js
+++ src/data/wordSets.js
@@ -4,10 +4,10 @@ export const wordSets = [
     name: 'Common Word Spelling',
     url: 'https://raw.githubusercontent.com/sudarshaana/learn/refs/heads/main/src/data/word_list.csv'
   },
-//   {
-//     id: 'common-misspelled',
-//     name: 'Common Misspelled Words',
-//     url: 'https://raw.githubusercontent.com/sudarshaana/learn/refs/heads/main/src/data/word_list.csv'
-//   }
+  {
+    id: 'common-ielts',
+    name: 'Common Words for IELTS',
+    url: 'https://raw.githubusercontent.com/sudarshaana/learn/refs/heads/main/src/data/word2.csv'
+  }
   // Add more word sets here
 ]
