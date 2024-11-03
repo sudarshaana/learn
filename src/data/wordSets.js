721982369d37cf27d0ccbd345c4ff18c55ed14c9
sudarshaana <sudarshaana@gmail.com>
2024-11-03 13:29:19 +0600

    added 1000 words set

--- src/data/wordSets.js
+++ src/data/wordSets.js
@@ -5,6 +5,11 @@ export const wordSets = [
     url: 'https://raw.githubusercontent.com/sudarshaana/learn/refs/heads/main/src/data/word_list.csv'
   },
   {
+    id: 'bengali-english-1000',
+    name: 'Common 1000 words (Bengali - English)',
+    url: 'https://raw.githubusercontent.com/sudarshaana/learn/refs/heads/main/src/data/words_1000.csv'
+  },
+  {
     id: 'common-ielts',
     name: 'Common Words for IELTS (300)',
     url: 'https://raw.githubusercontent.com/sudarshaana/learn/refs/heads/main/src/data/word2.csv'
@@ -14,6 +19,7 @@ export const wordSets = [
     name: 'Common One Word Substitution (200)',
     url: 'https://raw.githubusercontent.com/sudarshaana/learn/refs/heads/main/src/data/one_word_sub.csv'
   }
+
 
   // Add more word sets here
 ]
